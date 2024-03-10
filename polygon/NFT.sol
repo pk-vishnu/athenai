2ed018a5d83b3ab116e37b09c6ae84f9ba82108c
Sashank Ravipati <sashankr04@gmail.com>
2024-03-10 12:11:10 +0530

    feat: polygon smart contract

---
+++ polygon/NFT.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable{
+
+    using Counters for Counters.Counter;
+    Counters.Counter public currentTokenId;
+
+    string public baseTokenURI;
+
+    constructor(address initialOwner) ERC721("World of Women", "NFT"){
+        Ownable.transferOwnership(initialOwner);
+        baseTokenURI = "";
+        
+    }
+
+    function mintNFT() public returns (uint256){
+        currentTokenId.increment();
+        uint256 newItemId = currentTokenId.current();
+        _safeMint(msg.sender, newItemId);
+        return newItemId;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner{
+        baseTokenURI = _baseTokenURI;
+    }
+    
+}
